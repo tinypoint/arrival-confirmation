04866afb0229dc2422c87fb194604a0f427e7a65
张泽zze <zze.zhang@qunar.com>
2018-07-21 02:22:56 +0800

    init: @vue/cli 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/html-indent': [
+      'error',
+      2
+    ],
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': [
+      'error',
+      'double'
+    ]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
